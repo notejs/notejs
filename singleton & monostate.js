db0605c49e90aa3555fcd53e6bd84c5a2065408c
notejs <gm731958576@gmail.com>
2013-03-29 14:14:39 +0800

    rename file
    rename facade to facade & mediator
    rename Singleton to singleton & monostate

--- singleton & monostate.js
+++ singleton & monostate.js
@@ -26,3 +26,11 @@ var myPhone2 = new MyPhone();
 myPhone1.sendMsg("13614516638", "哈哈"); //将信息从13JQQQQKKAA发送到13614516638 
 myPhone2.sendMsg("13895772644", "呵呵"); //将信息从13JQQQQKKAA发送到13895772644 
 console.log(myPhone1 === myPhone2); //true
+
+
+
+/**
+ * Monostate模式。
+ * 
+ * 该模式实现功能与单例模式类似，但是实现原理却大不相同，该模式一般通过共享相同变量来实现。
+ */
